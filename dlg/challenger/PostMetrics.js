9240777b8607e6ea16e6f2385f0e2a02ad106a12
nicolasances <nicolas.matteazzi@gmail.com>
2020-02-29 18:24:01 +0100

    log

--- dlg/challenger/PostMetrics.js
+++ dlg/challenger/PostMetrics.js
@@ -17,11 +17,11 @@ exports.do = function(request) {
     if (!request.params.id) {failure({code: 400, message: 'Missing the "id" of the challenger in the path'}); return; }
     if (!body.metrics || body.metrics.length == 0) {failure({code: 400, message: 'Missing "metrics" in the body.'}); return;}
 
-    console.log(body);
-    
     metrics = body.metrics;
 
     for (metric in metrics) {
+      console.log(metric);
+      console.log(metric.name);
       if (!metric.name) {failure({code: 400, message: 'One of the metrics missed the "name" attribute'}); return;}
       if (!metric.value) {failure({code: 400, message: 'One of the metrics missed the "value" attribute'}); return;}
     }
