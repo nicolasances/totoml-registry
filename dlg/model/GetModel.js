d4358d26cb1a4330ba58cc54e406d5a5effb2b79
nicolasances <nicolas.matteazzi@gmail.com>
2020-03-22 14:06:54 +0100

    removed logs

--- dlg/model/GetModel.js
+++ dlg/model/GetModel.js
@@ -10,12 +10,7 @@ exports.do = function(req) {
 
     return MongoClient.connect(config.mongoUrl, function(err, db) {
 
-      console.log('Received GET on a model');
-      console.log(req);
-      
       db.db(config.dbName).collection(config.collections.models).find({name: req.params.name}).toArray(function(err, array) {
-
-        console.log(array[0]);
 
         db.close();
 
