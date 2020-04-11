58697269bfca0f5a3cadc41ab98a282e014020aa
nicolasances <nicolas.matteazzi@gmail.com>
2020-04-11 16:44:16 +0200

    logs

--- dlg/model/PostModel.js
+++ dlg/model/PostModel.js
@@ -9,6 +9,8 @@ exports.do = function(request) {
 
   let body = request.body;
 
+  console.log(body);
+
   return new Promise(function(success, failure) {
 
     // Some validation
@@ -18,10 +20,18 @@ exports.do = function(request) {
 
       updateModel = converter.updateModel(body)
 
+      console.log(updateModel);
+
       db.db(config.dbName).collection(config.collections.models).updateOne({name: body.name}, updateModel, {upsert: true}, function(err, res) {
+
+        console.log("Upserted model");
+        console.log(res);
+        
 
         // Read back the model
         getModel.do({params: {name: body.name}}).then((data) => {
+
+          console.log(data);
           
           db.close();
           
